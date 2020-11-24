78716f113a2d2fd60845d250e7d9b9e81b6a01a3
ca3hh <carter.angelus@richmond.edu>
2020-11-24 18:42:33 -0500

    added set method to trafficLight

--- trafficLight.h
+++ trafficLight.h
@@ -1,20 +1,26 @@
 #ifndef __TRAFFICLIGHT_H__
 #define __TRAFFICLIGHT_H__
 
+#include <string>
+using namespace std;
+
 class trafficLight{
 protected:
 	int G;
 	int Y;
 	int R;
+	string color; 
 
 public:
 	int countDown;
 
 	trafficLight();
-	trafficLight(int yLight, int gLight);
+	trafficLight(int gLight, int yLight, int rLight);
 
 	~trafficLight();
-
+	
+	void setColor(string c);
+	
 };
 
 #endif	
