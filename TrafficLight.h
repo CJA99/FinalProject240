f30ba4dfbe2ca752f47e10f916093d3e160c8416
CJA99 <71102110+CJA99@users.noreply.github.com>
2020-12-02 22:07:57 -0600

    Update TrafficLight.h
    added comments

--- TrafficLight.h
+++ TrafficLight.h
@@ -7,21 +7,23 @@ using namespace std;
 
 class TrafficLight{
 private:
-    int green;
-    int yellow;
-    int red;
-    LightColor color;
-    int countDown;
+    int green;          // length of a green light
+    int yellow;         // length of a yellow light
+    int red;            // length of a red light
+    LightColor color;   // the color of the light
+    int countDown;      // the time left on the light 
 
 public:
 
+    // constructors
     TrafficLight();
     TrafficLight(int gLight, int yLight, int rLight, LightColor color);
     ~TrafficLight(){}
 
-    inline LightColor getColor(){ return color; }
-    inline int changeTime(){ return countDown; }
-    void decrement();
+    
+    inline LightColor getColor(){ return color; }   // returns the color of the light
+    inline int changeTime(){ return countDown; }    // returns the amount of time before the light changes
+    void decrement();                               // counts down the time left on the light
 };
 
 #endif
